a9aa115a6c524de0be5b8cf0a65d7a070caaed46
ColineVL <coline.vanleeuwen@gmail.com>
2021-11-30 18:12:01 +0100

    exo1

--- threadpool_src/tp_web_1.c
+++ threadpool_src/tp_web_1.c
@@ -21,15 +21,26 @@ char response[] = "HTTP/1.1 200 OK\r\n"
 /******************************************************************************/
 /* processing the request                                                     */
 /******************************************************************************/
-void process_request ( /* to be completed  */  ) {
+void process_request (void *requete ) {
+  int *client_fd = (int *) requete;
+  int bufsize = 1024;
+  char *buffer = malloc(bufsize);
 
+  if (*client_fd == -1)
+    perror("accept");
+
+  recv(*client_fd, buffer, bufsize, 0);
+  printf ("%s\n", buffer);
+  send(*client_fd, response, sizeof(response), 0);
+  close(*client_fd);
+  free (buffer);
 }
 
 /******************************************************************************/
 /* dispatching the requests                                                   */
 /******************************************************************************/
-void dispatch_requests ( /* to be completed  */ ) {
- 
+void dispatch_requests (pthread_pool_t *thread_pool, void *requete) {
+  pthread_pool_exec(thread_pool,process_request,requete);
 }
 
 /******************************************************************************/
@@ -38,24 +49,52 @@ void dispatch_requests ( /* to be completed  */ ) {
 
 int main(int argc, char **argv) {
   
-    printf  ("\x1B[32m %s \x1B[0m%s", "[MAIN]", "Start processing\n");
+  printf  ("\x1B[32m %s \x1B[0m%s", "[MAIN]", "Start processing\n");
 
-    /*--------------------------------------------------------------------------*/
-    /* Initialize a thread pool of size 10                                       */
-    /*--------------------------------------------------------------------------*/
-    pthread_pool_t *thread_pool = pthread_pool_init (10);  
-    
-     /* Leave some time for the threadpool to finish its initialization */
-    sleep (1);
+  /*--------------------------------------------------------------------------*/
+  /* Initialize a thread pool of size 10                                       */
+  /*--------------------------------------------------------------------------*/
+  pthread_pool_t *thread_pool = pthread_pool_init (10);  
+  if (thread_pool == NULL)
+  perror ("ERROR: failed to create the thread pool, ");
+  printf ("\x1B[32m %s \x1B[0m%s", "[MAIN]", "Start processing\n");
+
+    /* Leave some time for the threadpool to finish its initialization */
+  sleep (1);
 
 
-    /*--------------------------------------------------------------------------*/
-    /* Initialize web server                                                    */
-    /*--------------------------------------------------------------------------*/
-  
-    /*--------------------------------------------------------------------------*/
-    /* Process incoming requests                                                */
-    /*--------------------------------------------------------------------------*/
- 
+  /*--------------------------------------------------------------------------*/
+  /* Initialize web server                                                    */
+  /*--------------------------------------------------------------------------*/
+  int one = 1, client_fd;
+  struct sockaddr_in svr_addr, from;
+  socklen_t sin_len = sizeof(from);
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0)
+    perror("socket");
+
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+
+  int port = 1234;
+  svr_addr.sin_family = AF_INET;
+  svr_addr.sin_addr.s_addr = INADDR_ANY;
+  svr_addr.sin_port = htons(port);
+
+  if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
+    close(sock);
+    perror("bind");
+  }
+
+  listen(sock, 5);
+
+  /*--------------------------------------------------------------------------*/
+  /* Process incoming requests                                                */
+  /*--------------------------------------------------------------------------*/
+  while (1) {
+    client_fd = accept(sock, (struct sockaddr *) &from, &sin_len);
+    dispatch_requests(thread_pool,(void *)& client_fd);
+  }
+
   return EXIT_SUCCESS;
 }
