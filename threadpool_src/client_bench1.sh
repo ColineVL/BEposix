a9242ff287b409aff47baa00c0bb654e0e05f5a1
tchinguiz <at.eskoulov@protonmail.com>
2021-12-14 20:09:45 +0100

    final release

--- threadpool_src/client_bench1.sh
+++ threadpool_src/client_bench1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-file="./test_data_log.txt"
+file="./res_log_arthur_lf.txt"
 [ -f $file ] && rm $file
 touch $file
 
@@ -8,7 +8,7 @@ for k in 50 100 150 200 250 300 # number of clients
 do
 
     echo "n_clients - $k" >> $file
-    for j in {1..10} # launch 10 times to compute a mean
+    for j in {1..2} # launch 10 times to compute a mean
     do
         time_start=$(date +%s.%N)
         for i in $( seq 1 $k ) # launch k clients (almost) simultaneously
@@ -19,6 +19,7 @@ do
         delta=$(echo "$time_end - $time_start" | bc)
         echo "launch_time - $delta" >> $file
         sleep 10
+        pkill -P $$
     done
     # python parser.py $file
 done
